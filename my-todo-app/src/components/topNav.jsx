405ff9cfa6ceb4e190c6977dbb04ade3936bc40e
MuritalaAzeezAdeniyi <azeezmuritalaadeniyi@gmail.com>
2024-09-17 09:17:18 +0100

    adding new function

--- my-todo-app/src/components/topNav.jsx
+++ my-todo-app/src/components/topNav.jsx
@@ -1,5 +1,8 @@
 import style from "./Style/topNav.module.css"
 import background from "../assets/task.jpg"
+import image from "../assets/foot2.avif"
+import photo from "../assets/foot3.avif"
+import todo from "../assets/todo1.jpg"
 import { Link } from "react-router-dom"
 const TopNav = () => {
     return (
@@ -15,16 +18,26 @@ const TopNav = () => {
                     {/* <Link to="/signUp"> */}
                         <button className={style['sign-up']}>SignUp</button>
                     {/* </Link> */}
-                    <button className={style['AddTask']}>AddTask</button>
+                    
                 </div>
             </div>
           <img className={style['background']} src={background} alt="Background" /> 
     
           <div>
             <h1 className={style['text']}>Schedule Your <br/> Daily Task With <br/><span className={style['mid']}>Best Todo App!</span> </h1>
+            <button className={style['AddTask']}>AddTask</button>
+            <div className={style['midtt']}><h5 className={style['midtext']}>Explore idea collection</h5></div>
+           <br/>
+          
           </div>
           <img className={style['background']} src={background} alt="Background" /> 
-        
+             
+             <br/>
+             <div className={style['midImag']}> <img className={style['image']} src={image} alt ={image} />
+             
+               <img className={style['photo']} src={photo} alt={photo}/>
+               <div className={style['midphoto']}><img className={style['todo']} src={todo} alt={todo}/></div>
+             </div>
         </div>
     )
 }
